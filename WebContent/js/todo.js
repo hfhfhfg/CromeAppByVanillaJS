01f3b782c9177f3b5190558a99ea39cda4a00aff
hfhfhfg <hfhfhfg5@gmail.com>
2020-05-12 00:02:37 +0900

    toDoList의 Array를 JSON.Stringify기법으로 localStorage에 저장하고 JSON.parse로 다시 오브젝트형태로 받아오고 toDoList를 화면에 paint 한다

--- WebContent/js/todo.js
+++ WebContent/js/todo.js
@@ -5,19 +5,42 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+
+function saveToDos(){
+	localStorage.setItem(TODOS_LS,JSON.stringify(toDos));
+}
+
 function paintToDo(text){
 	
 	const li = document.createElement("li");
 	const delBtn = document.createElement("button");
+	const span = document.createElement("span");
+	const newId = toDos.length +1;
+	
+	li.id = newId;
+	
 	delBtn.innerText = "XXX";
 	
 	toDoInput.value = "";
-	const span = document.createElement("span");
+	
+	
 	span.innerText = text;
 	
 	li.appendChild(delBtn);
 	li.appendChild(span);
 	toDoList.appendChild(li);
+	
+	const toDoObj = {
+			
+			text: text,
+			id: newId
+	}
+	
+	toDos.push(toDoObj);
+	
+	saveToDos();
 }
 
 function handleSubmit(event){
@@ -29,9 +52,17 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-	const toDos = localStorage.getItem(TODOS_LS);
+	const loadedToDos = localStorage.getItem(TODOS_LS);
 	
 	if(toDos !== null){
+		const parsedToDos = JSON.parse(loadedToDos);
+		
+		parsedToDos.forEach(function(toDo){
+			
+			paintToDo(toDo.text);
+		});
+		
+		
 		
 	}
 }
